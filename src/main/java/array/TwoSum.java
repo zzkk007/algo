4694c96d4e27fe8b8a1f7db4d2f67a481e208e75
huanongzhangkun <zhangkun@chinahuanong.com.cn>
2020-12-08 20:22:18 +0800

    数组

--- src/main/java/array/TwoSum.java
+++ src/main/java/array/TwoSum.java
@@ -24,10 +24,10 @@ public class TwoSum {
      */
     private static int[] testOne(int[] nums, int target) {
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 System.out.println("i:" + nums[i] + " j:" + nums[j]);
-                if (nums[i] + nums[j] == target){
+                if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
             }
@@ -42,7 +42,7 @@ public class TwoSum {
         int[] intArray = new int[]{2, 11, 15, 7};
         int[] ints = testOne(intArray, target);
         System.out.println("====================");
-        for (int i : ints){
+        for (int i : ints) {
             System.out.println(i);
         }
 
