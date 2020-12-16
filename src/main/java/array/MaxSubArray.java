79aec09de1f270579671ff70cd4823d7efd8cf88
huanongzhangkun <zhangkun@chinahuanong.com.cn>
2020-12-16 22:27:10 +0800

    求最大子数组

--- src/main/java/array/MaxSubArray.java
+++ src/main/java/array/MaxSubArray.java
@@ -19,21 +19,24 @@ package array;
 public class MaxSubArray {
 
 
-    public static int[] testOne(int[] nums) {
+    public static int testOne(int[] nums) {
 
-        return nums;
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
     }
+
 
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{2, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
 
-        testOne(nums);
-        System.out.println("====================" + nums);
-        for (int i : nums) {
-            System.out.println(i);
-        }
+        int num = testOne(nums);
+        System.out.println("====================" + num);
 
     }
 }
